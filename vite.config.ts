f9b041e02be2d6588b611cbdc73a317b7afe5278
John Dunglao <jfsdunglao@gmail.com>
2025-11-27 14:23:15 +0800

    init react project

---
+++ vite.config.ts
@@ -0,0 +1,38 @@
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vite.dev/config/
+export default defineConfig(({ mode }) => {
+  const isProduction = mode === "production";
+  console.debug('Running React app on', mode, 'mode')
+  
+  const env = loadEnv(mode, process.cwd())
+  console.debug('Loaded environment variables')
+  
+  return {
+    plugins: [
+      react()
+    ],
+    preview: {
+      port: parseInt(env.VITE_PREVIEW_HOST || '0', 10),
+      allowedHosts: env.VITE_PREVIEW_HOST?.split(',') || []
+    },
+    server: {
+      port: parseInt(env.VITE_APP_PORT || '0', 10),
+      allowedHosts: env.VITE_APP_HOST?.split(',') || [],
+      host: true,
+      strictPort: true,
+      watch: {
+        usePolling: true
+      },
+      hmr: {
+        host: 'localhost',
+        clientPort: parseInt(env.VITE_APP_PORT || '0', 10),
+      },
+    },
+    esbuild: {
+      pure: isProduction ? ["console.log", "console.debug"] : [],
+      drop: isProduction ? ["debugger"] : [],
+    },
+  }
+})
